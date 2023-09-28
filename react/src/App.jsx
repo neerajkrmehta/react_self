5cdf101016c8eec99f7a63b22bfe0366cddbe084
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-09-28 12:26:32 +0530

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Profile from './Components/Profile'
+
+function App() {
+
+
+
+return(
+
+<Profile></Profile>
+
+)
+}
+
+
+
+
+export default App
