f3324a05d360529994ed966a502e393da12786b8
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-09-29 15:44:29 +0530

    onClick

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,26 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Profile from './Components/Profile'
-import people from './Components/Data/Data'
+import Button from './Components/Button/Button'
+// import people from './Components/Data/Data'
 
 function App() {
 
- const chemists = people.filter((person) => person.profession === "chemist");
-
- const listItems = chemists.map((person) => (
-   <li>
-   
-     <p>
-     
-       <b>{person.name}:</b>
-       {" " + person.profession + " "}
-       known for {person.accomplishment}
-     </p>
-   </li>
- ));
+ 
 return (
   <>
-   <ul>{listItems}</ul>
+    <Button 
+    onclickplay={()=>alert('playing')}
+    onclickstop={()=>alert('stoping')}
+     />
   </>
 );
 }
