fb2c4ab78309a16a417670ffd8bf2deecdbab501
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-10-01 18:35:52 +0530

    Form

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -10,38 +10,79 @@ import getFinalState from './Components/ProcessQueue'
 
 function App() {
 
-  const [position, setPosition] = useState({
-    x: 0,
-    y: 0,
+   const [person, setPerson] = useState({
+    name: 'Niki de Saint Phalle',
+    artwork: {
+      title: 'Blue Nana',
+      city: 'Hamburg',
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    }
   });
 
+  function handleNameChange(e) {
+    setPerson({
+      ...person,
+      name: e.target.value
+    });
+  }
+
+  function handleTitleChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value
+      }
+    });
+  }
+
+  function handleCityChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        city: e.target.value
+      }
+    });
+  }
+
+  function handleImageChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value
+      }
+    });
+  }
+
 return (
-  <div
-    onPointerMove={(e) => {
-      setPosition({
-        x: e.clientX,
-        y: e.clientY,
-      });
-    }}
-    style={{
-      position: "relative",
-      width: "100vw",
-      height: "100vh",
-    }}
-  >
-    <div
-      style={{
-        position: "absolute",
-        backgroundColor: "red",
-        borderRadius: "50%",
-        transform: `translate(${position.x}px, ${position.y}px)`,
-        left: -10,
-        top: -10,
-        width: 20,
-        height: 20,
-      }}
-    />
-  </div>
+  <>
+    <label>
+      Name:
+      <input value={person.name} onChange={handleNameChange} />
+    </label>
+    <label>
+      Title:
+      <input value={person.artwork.title} onChange={handleTitleChange} />
+    </label>
+    <label>
+      City:
+      <input value={person.artwork.city} onChange={handleCityChange} />
+    </label>
+    <label>
+      Image:
+      <input value={person.artwork.image} onChange={handleImageChange} />
+    </label>
+    <p>
+      <i>{person.artwork.title}</i>
+      {" by "}
+      {person.name}
+      <br />
+      (located in {person.artwork.city})
+    </p>
+    <img src={person.artwork.image} alt={person.artwork.title} />
+  </>
 );
 }
 
