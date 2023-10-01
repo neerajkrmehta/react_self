92efb54206cf655a1618ac808e361ea13a776c4e
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-10-01 15:54:04 +0530

    ChallangeProblem

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,23 +5,59 @@ import './App.css'
 import Profile from './Components/Profile'
 import Button from './Components/Button/Button'
 import list from './Components/Data/Data'
+import getFinalState from './Components/ProcessQueue'
+
+
+
+function increment(n) {
+  return n + 1;
+}
+increment.toString = () => "n => n+1";
 
 
 function App() {
-function handleClick() {
-  let bodyStyle = document.body.style;
-  if (bodyStyle.backgroundColor === "black") {
-    bodyStyle.backgroundColor = "white";
-  } else {
-    bodyStyle.backgroundColor = "black";
-  }
-}
+
 return (
   <>
-    <button onClick={handleClick}>Toggle the lights</button>
+    <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
+    <hr />
+    <TestCase
+      baseState={0}
+      queue={[increment, increment, increment]}
+      expected={3}
+    />
+    <hr />
+    <TestCase baseState={0} queue={[5, increment]} expected={6} />
+    <hr />
+    <TestCase baseState={0} queue={[5, increment, 42]} expected={42} />
   </>
 );
 }
+function TestCase({ baseState, queue, expected }) {
+  const actual = getFinalState(baseState, queue);
+  return (
+    <>
+      <p>
+        Base state: <b>{baseState}</b>
+      </p>
+      <p>
+        Queue: <b>[{queue.join(", ")}]</b>
+      </p>
+      <p>
+        Expected result: <b>{expected}</b>
+      </p>
+      <p
+        style={{
+          color: actual === expected ? "green" : "red",
+        }}
+      >
+        Your result: <b>{actual}</b> (
+        {actual === expected ? "correct" : "wrong"})
+      </p>
+    </>
+  );
+}
+
 
 
 
---
+++ react/src/Components/ProcessQueue.jsx
@@ -0,0 +1,16 @@
+ function getFinalState(baseState, queue) {
+  let finalState = baseState;
+
+  for (let update of queue) {
+    if (typeof update === "function") {
+      // Apply the updater function.
+      finalState = update(finalState);
+    } else {
+      // Replace the next state.
+      finalState = update;
+    }
+  }
+
+  return finalState;
+}
+export default getFinalState;
