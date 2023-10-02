472e23b6eeccc800a738bc6133e8f58f7e16e052
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-10-02 20:38:23 +0530

    ArtBucketList

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,39 +1,70 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Profile from "./Components/Profile";
-import Button from "./Components/Button/Button";
-import list from "./Components/Data/Data";
-import getFinalState from "./Components/ProcessQueue";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Profile from './Components/Profile'
+import Button from './Components/Button/Button'
+import list from './Components/Data/Data'
+import getFinalState from './Components/ProcessQueue'
 
-let nextId = 0;
+let nextId = 3;
+const initialList = [
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Landscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
+];
+
 
 function App() {
-  const [name, setName] = useState("");
-  const [artists, setArtists] = useState([]);
+const [myList, setMyList] = useState(initialList);
+const [yourList, setYourList] = useState(initialList);
 
+function handleToggleMyList(artworkId, nextSeen) {
+  const myNextList = [...myList];
+  const artwork = myNextList.find((a) => a.id === artworkId);
+  artwork.seen = nextSeen;
+  setMyList(myNextList);
+}
+
+function handleToggleYourList(artworkId, nextSeen) {
+  const yourNextList = [...yourList];
+  const artwork = yourNextList.find((a) => a.id === artworkId);
+  artwork.seen = nextSeen;
+  setYourList(yourNextList);
+}
+ 
+
+return (
+  <>
+    <h1>Art Bucket List</h1>
+    <h2>My list of art to see:</h2>
+    <ItemList artworks={myList} onToggle={handleToggleMyList} />
+    <h2>Your list of art to see:</h2>
+    <ItemList artworks={yourList} onToggle={handleToggleYourList} />
+  </>
+);
+}
+function ItemList({ artworks, onToggle }) {
   return (
-    <>
-      <h1>Inspiring sculptors:</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button
-        onClick={() => {
-          artists.push({
-            id: nextId++,
-            name: name,
-          });
-        }}
-      >
-        Add
-      </button>
-      <ul>
-        {artists.map((artist) => (
-          <li key={artist.id}>{artist.name}</li>
-        ))}
-      </ul>
-    </>
+    <ul>
+      {artworks.map((artwork) => (
+        <li key={artwork.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={artwork.seen}
+              onChange={(e) => {
+                onToggle(artwork.id, e.target.checked);
+              }}
+            />
+            {artwork.title}
+          </label>
+        </li>
+      ))}
+    </ul>
   );
 }
 
-export default App;
+
+
+export default App
