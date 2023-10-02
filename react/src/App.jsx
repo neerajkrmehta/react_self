da862f5ad0c9acef2cb3baa3c4ac06af3cf46b2a
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-10-02 10:10:16 +0530

    push In Array

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,92 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Profile from './Components/Profile'
-import Button from './Components/Button/Button'
-import list from './Components/Data/Data'
-import getFinalState from './Components/ProcessQueue'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Profile from "./Components/Profile";
+import Button from "./Components/Button/Button";
+import list from "./Components/Data/Data";
+import getFinalState from "./Components/ProcessQueue";
 
+let nextId = 0;
 
 function App() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
 
-   const [person, setPerson] = useState({
-    name: 'Niki de Saint Phalle',
-    artwork: {
-      title: 'Blue Nana',
-      city: 'Hamburg',
-      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
-    }
-  });
-
-  function handleNameChange(e) {
-    setPerson({
-      ...person,
-      name: e.target.value
-    });
-  }
-
-  function handleTitleChange(e) {
-    setPerson({
-      ...person,
-      artwork: {
-        ...person.artwork,
-        title: e.target.value
-      }
-    });
-  }
-
-  function handleCityChange(e) {
-    setPerson({
-      ...person,
-      artwork: {
-        ...person.artwork,
-        city: e.target.value
-      }
-    });
-  }
-
-  function handleImageChange(e) {
-    setPerson({
-      ...person,
-      artwork: {
-        ...person.artwork,
-        image: e.target.value
-      }
-    });
-  }
-
-return (
-  <>
-    <label>
-      Name:
-      <input value={person.name} onChange={handleNameChange} />
-    </label>
-    <label>
-      Title:
-      <input value={person.artwork.title} onChange={handleTitleChange} />
-    </label>
-    <label>
-      City:
-      <input value={person.artwork.city} onChange={handleCityChange} />
-    </label>
-    <label>
-      Image:
-      <input value={person.artwork.image} onChange={handleImageChange} />
-    </label>
-    <p>
-      <i>{person.artwork.title}</i>
-      {" by "}
-      {person.name}
-      <br />
-      (located in {person.artwork.city})
-    </p>
-    <img src={person.artwork.image} alt={person.artwork.title} />
-  </>
-);
+  return (
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button
+        onClick={() => {
+          artists.push({
+            id: nextId++,
+            name: name,
+          });
+        }}
+      >
+        Add
+      </button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-
-
-
-export default App
+export default App;
