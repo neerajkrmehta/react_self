1ffe03f5ce180df0cd6cfbbe59941b9726230471
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-10-05 16:21:36 +0530

    ...

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -9,69 +9,39 @@ import getFinalState from './Components/ProcessQueue'
 
 
 function App() {
-  const [answer, setAnswer] = useState("");
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState("typing");
-
-  if (status === "success") {
-    return <h1>That's right!</h1>;
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus("submitting");
-    try {
-      await submitForm(answer);
-      setStatus("success");
-    } catch (err) {
-      setStatus("typing");
-      setError(err);
-    }
-  }
-
-  function handleTextareaChange(e) {
-    setAnswer(e.target.value);
-  }
-
-
-return (
-  <>
-    <h2>City quiz</h2>
-    <p>
-      In which city is there a billboard that turns air into drinkable water?
-    </p>
-    <form onSubmit={handleSubmit}>
-      <textarea
-        value={answer}
-        onChange={handleTextareaChange}
-        disabled={status === "submitting"}
-      />
-      <br />
-      <button disabled={answer.length === 0 || status === "submitting"}>
-        Submit
-      </button>
-      {error !== null && <p className="Error">{error.message}</p>}
-    </form>
-  </>
-);
-}
-
-
-function submitForm(answer) {
-  // Pretend it's hitting the network.
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let shouldError = answer.toLowerCase() !== "lima";
-      if (shouldError) {
-        reject(new Error("Good guess but a wrong answer. Try again!"));
-      } else {
-        resolve();
-      }
-    }, 1500);
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
   });
+  return (
+    <div
+      onPointerMove={(e) => {
+        setPosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
+      }}
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "red",
+          borderRadius: "50%",
+          transform: `translate(${position.x}px, ${position.y}px)`,
+          left: -10,
+          top: -10,
+          width: 20,
+          height: 20,
+        }}
+      />
+    </div>
+  );
 }
-
-
 
 
 export default App
