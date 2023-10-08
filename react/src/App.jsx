bace6c6e96cf3d77c68635320038ccaf808a7a0e
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-10-08 15:51:42 +0530

    ...

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import AddTask from "./AddTask.js";
 import TaskList from "./TaskList.js";
 
 export default function TaskApp() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
 
   function handleAddTask(text) {
-    setTasks([
-      ...tasks,
-      {
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    ]);
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
   }
 
   function handleChangeTask(task) {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
-        } else {
-          return t;
-        }
-      })
-    );
+    dispatch({
+      type: "changed",
+      task: task,
+    });
   }
 
   function handleDeleteTask(taskId) {
-    setTasks(tasks.filter((t) => t.id !== taskId));
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
   }
 
   return (
@@ -43,6 +38,36 @@ export default function TaskApp() {
       />
     </>
   );
+}
+
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
 }
 
 let nextId = 3;
